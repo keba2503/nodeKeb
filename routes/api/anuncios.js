8dc3428d7d3f41f681b0fef2bbd88e0a47d22ced
Karen Borrero <keba2503@gmail.com>
2020-04-17 09:07:01 +0100

    id search

--- routes/api/anuncios.js
+++ routes/api/anuncios.js
@@ -5,12 +5,25 @@ const router = express.Router();
 
 const Anuncios = require('../../models/Anuncios');
 
-router.get('/', (req, res, next) => {
-    Anuncios.find().exec((err, docs) => {
-      res.json(docs);
-    });
-  });
+router.get('/', async (req, res, next) => {
+    try {
+        const docs = await Anuncios.find();
+        res.json(docs);
+    } catch(err) {
+        next(err);
+    }
+});
+
+//Id search
+router.get('/:id', async (req, res, next) => {
+    try {
+        const _id = req.params.id;
+        const anuncios = await Anuncios.findOne({ _id: _id });
+        res.json({ result: anuncios });
+    } catch (err) {
+        next(err);
+    }
+});
 
   module.exports = router;
 
-  
